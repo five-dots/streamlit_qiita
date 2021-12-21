6ac2fb650dfb7cc5ca43c5b1e95be8646a056277
Shun Asai <syun.asai@gmail.com>
2021-12-21 14:33:57 +0900

    Add title/subtitle

--- demo.py
+++ demo.py
@@ -8,4 +8,7 @@ df = pd.DataFrame(
     np.random.randn(1000, 2) / [100, 100] + [lat, lon],
     columns=['lat', 'lon']
 )
+
+st.title("位置情報を共有する最も簡単な方法")
+st.subheader("Location Tech Advent Calendar 2021 by LBMA Japan")
 st.map(df)
